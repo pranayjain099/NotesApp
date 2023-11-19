f99888d4546bd34c7e1e8ec90ee636a7779ba2cc
pranayjain099 <pranayjain2001@gmail.com>
2023-11-19 19:26:58 +0530

    Connected with mysql'

--- notesapp.php
+++ notesapp.php
@@ -16,6 +16,44 @@ $conn = mysqli_connect($servername, $username, $password, $database);
 if (!$conn) {
     die("Sorry we failed to connect: " . mysqli_connect_error());
 }
+
+if (isset($_GET['delete'])) {
+    $sno = $_GET['delete'];
+    $delete = true;
+    $sql = "DELETE FROM `notes` WHERE `sno` = $sno";
+    $result = mysqli_query($conn, $sql);
+}
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (isset($_POST['snoEdit'])) {
+        // Update the record
+        $sno = $_POST["snoEdit"];
+        $title = $_POST["titleEdit"];
+        $description = $_POST["descriptionEdit"];
+
+        // Sql query to be executed
+        $sql = "UPDATE `notes` SET `title` = '$title' , `description` = '$description' WHERE `notes`.`sno` = $sno";
+        $result = mysqli_query($conn, $sql);
+        if ($result) {
+            $update = true;
+        } else {
+            echo "We could not update the record successfully";
+        }
+    } else {
+        $title = $_POST["title"];
+        $description = $_POST["description"];
+
+        // Sql query to be executed
+        $sql = "INSERT INTO `notes` (`title`, `description`) VALUES ('$title', '$description')";
+        $result = mysqli_query($conn, $sql);
+
+
+        if ($result) {
+            $insert = true;
+        } else {
+            echo "The record was not inserted successfully because of this error ---> " . mysqli_error($conn);
+        }
+    }
+}
 ?>
 
 <!-- HTML -->
@@ -80,6 +118,39 @@ if (!$conn) {
             </div>
             <button type="submit" class="btn btn-primary">Add Note</button>
         </form>
+    </div>
+
+    <!-- Displaying notes -->
+    <div class="container my-4">
+        <table class="table" id="myTable">
+            <thead>
+                <tr>
+                    <th scope="col">S.No</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $sql = "SELECT * FROM `notes`";
+                $result = mysqli_query($conn, $sql);
+                $sno = 0;
+                while ($row = mysqli_fetch_assoc($result)) {
+                    $sno = $sno + 1;
+                    echo
+                        "<tr>
+                        <th scope='row'>" . $sno . "</th>
+                        <td>" . $row['title'] . "</td>
+                        <td>" . $row['description'] . "</td>
+                        <td> <button class='edit btn btn-sm btn-primary' id=" . $row['sno'] . ">Edit</button> <button class='delete btn btn-sm btn-primary' id=d" . $row['sno'] . ">Delete</button>  </td>
+                    </tr>";
+                }
+                ?>
+
+
+            </tbody>
+        </table>
     </div>
 
     <!-- Bootstrap script -->
